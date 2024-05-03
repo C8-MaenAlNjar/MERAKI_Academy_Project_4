9729e1e7c4316f583209e055c343e66d7fc6c1c0
MaenAlNjar <maen.njarr@gmail.com>
2024-05-03 23:10:03 +0300

    login2

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -2,13 +2,35 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+  const tokenFromHeader = req.headers.authorization;
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is not Valid!" });
-    req.userId = payload.id;
+  if ( !tokenFromHeader)
+    return res.status(401).json({ message: "Not Authenticated!" });
 
-    next();
-  });
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET_KEY,
+      async (err, payload) => {
+        if (err)
+          return res
+            .status(403)
+            .json({ message: "Token from cookies is not valid!" });
+        req.userId = payload.id;
+        next();
+      }
+    );
+  }
+  // Verify token from headers
+  else if (tokenFromHeader) {
+    const token = tokenFromHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+      if (err)
+        return res
+          .status(403)
+          .json({ message: "Token from headers is not valid!" });
+      req.userId = payload.id;
+      next();
+    });
+  }
 };
-
