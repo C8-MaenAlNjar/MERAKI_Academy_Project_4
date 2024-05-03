41d69337804c682904344fc06905d877e89b3b73
MaenAlNjar <maen.njarr@gmail.com>
2024-05-03 07:35:46 +0300

    client

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://meraki-academy-project-4-83f6.onrender.com",
+  baseURL: "https://meraki-academy-project-4-83f6.onrender.com/api",
   withCredentials: true,
 });
 
