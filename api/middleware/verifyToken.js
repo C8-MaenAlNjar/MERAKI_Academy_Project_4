4c417d70999cb3ae84be3dd0b5c17085c761e266
MaenAlNjar <maen.njarr@gmail.com>
2024-05-04 07:40:41 +0300

    auth2

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -2,13 +2,28 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
+  const authHeader = req.headers.authorization;
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is not Valid!" });
-    req.userId = payload.id;
+  if (!authHeader && !token) {
+    return res.status(401).json({ message: "Not Authenticated!" });
+  }
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+      if (err) return res.status(403).json({ message: "Token is not Valid!" });
+      req.userId = payload.id;
 
-    next();
-  });
+      next();
+    });
+  } else if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+      if (err) return res.status(403).json({ message: "Token is not Valid!" });
+      req.userId = payload.id;
+
+      next();
+    });
+  }
 };
--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -6,11 +6,8 @@ import { Navigate, Outlet } from "react-router-dom";
 export const postLoader = async ({ request, params }) => {
   try {
     const postPromise = await apiRequest.get("/post");
-    const chatPromise = await apiRequest.get("/chat");
-console.log(chatPromise);
     return defer({
       postResponse: postPromise,
-      chatResponse: chatPromise,
     });
   } catch (error) {
     console.log(error);
