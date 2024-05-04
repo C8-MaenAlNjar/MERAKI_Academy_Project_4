01dfd53dbec91d5b665a9fc93e6f1b46c067f233
MaenAlNjar <maen.njarr@gmail.com>
2024-05-04 14:43:00 +0300

    corsError6

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,28 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
   const authHeader = req.headers.authorization;
-
-
+  console.log(authHeader);
   if (!authHeader) {
-    return res.status(401).json({ message: "Not Authenticated!" });
+    return res
+      .status(401)
+      .json({ message: "Not Authenticated noHeader!", authHeader: authHeader });
   }
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-      if (err) return res.status(403).json({ message: "Token is not Valid!" });
-      req.userId = payload.id;
 
-      next();
-    });
-  } else if (authHeader) {
-    const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1];
+  console.log(token, "token");
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-      if (err) return res.status(403).json({ message: "Token is not Valid!" });
-      req.userId = payload.id;
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is not Valid!" });
+    req.userId = payload.id;
 
-      next();
-    });
-  }
+    next();
+  });
 };
