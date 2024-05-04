476088f19a42860c6005c3055eeff11dfecbd8c0
MaenAlNjar <maen.njarr@gmail.com>
2024-05-04 14:09:37 +0300

    corsError3

--- api/app.js
+++ api/app.js
@@ -26,7 +26,7 @@ const corsOptions = {
   },
   credentials: true,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 config({ path: ".env" });
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://meraki-academy-project-4-83f6.onrender.com",
+  baseURL: "http://localhost:8800",
   withCredentials: true,
 });
 
